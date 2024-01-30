485cae2c68542ef6070be7d7460bf6487656e779
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-30 23:14:32 +0100

    Create react components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Provider } from "react-redux";
+import Greeting from "./Greeting";
+
+const App = () => { 
+    return (
+        // <Provider>
+            <Greeting />
+        // </Provider>
+    )
+}
+
+export default App;
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Greeting = () => {
+    return (
+        <div>This is my message component!</div>
+    )
+}
+
+export default Greeting;
